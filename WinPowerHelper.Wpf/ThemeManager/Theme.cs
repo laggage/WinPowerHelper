60736c06f7761bd3d3552d0e96647ef6aadb933c
laggage <1634205628@qq.com>
2020-01-30 01:47:28 +0800

    完善主题功能

---
+++ WinPowerHelper.Wpf/ThemeManager/Theme.cs
@@ -0,0 +1,33 @@
+﻿namespace WinPowerHelper.Wpf.ThemeManager
+{
+    using System.Windows;
+    using System.Windows.Media;
+
+    public class Theme
+    {
+        public Theme()
+        {
+        }
+
+        public Theme(ResourceDictionary resources)
+        {
+            Name = resources[ThemeNameKey] as string;
+            DisplayName = resources[ThemeDisplayNameKey] as string;
+            BaseColorScheme = resources[ThemeBaseColorSchemeKey] as string;
+            ColorScheme = resources[ThemeColorSchemeKey] as string;
+            ShowcaseBrush = resources[ThemeShowcaseBrushKey] as Brush;
+        }
+
+        public const string ThemeNameKey = "Theme.Name";
+        public const string ThemeDisplayNameKey = "Theme.DisplayName";
+        public const string ThemeBaseColorSchemeKey = "Theme.BaseColorScheme";
+        public const string ThemeColorSchemeKey = "Theme.ColorSchema";
+        public const string ThemeShowcaseBrushKey = "Theme.ShowcaseBrush";
+
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public string BaseColorScheme { get; set; }
+        public string ColorScheme { get; set; }
+        public Brush ShowcaseBrush { get; set; }
+    }
+}
---
+++ WinPowerHelper.Wpf/ThemeManager/ThemeManager.cs
@@ -0,0 +1,56 @@
+﻿namespace WinPowerHelper.Wpf.ThemeManager
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Resources;
+    using System.Text;
+    using System.Windows;
+
+    public static class ThemeManager
+    {
+        static ThemeManager()
+        {
+            Themes = new List<Theme>();
+        }
+
+        private static List<Theme> _themes;
+        public static List<Theme> Themes
+        {
+            get
+            {
+                EnsureThemes();
+                return _themes;
+            }
+            private set
+            {
+                _themes = value;
+            }
+        }
+
+        private static void EnsureThemes()
+        {
+            if (Themes.Count > 0) return;
+            var assembly = typeof(ThemeManager).Assembly;
+            var assemblyName = assembly.GetName();
+            var resourceNames = assembly.GetManifestResourceNames();
+
+            foreach (var resourceName in resourceNames)
+            {
+                if (!resourceName.EndsWith(".g.resources", StringComparison.OrdinalIgnoreCase))
+                    continue;
+
+                using (var reader = new ResourceReader(assembly.GetManifestResourceStream(resourceName)))
+                {
+                    foreach (DictionaryEntry entry in reader)
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+                
+                
+            }
+        }
+
+    }
+}
