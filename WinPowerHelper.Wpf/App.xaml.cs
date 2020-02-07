6390c0badde11b6ead598acc94da59daf9ed27e3
laggage <laggage-la@outlook.com>
2020-02-08 01:13:44 +0800

    实现应用程序单例运行

--- WinPowerHelper.Wpf/App.xaml.cs
+++ WinPowerHelper.Wpf/App.xaml.cs
@@ -1,11 +1,32 @@
 ﻿namespace WinPowerHelper
 {
+    using System;
+    using System.Threading;
     using System.Windows;
+    using WinPowerHelper.Wpf.Interaction;
 
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Startup += (e, args) =>
+            {
+                _mutex = new Mutex(true, "WinPowerHelper", out bool isNew);
+                if (!isNew)
+                {
+                    NativeMethods.PostMessage(
+                        (IntPtr)NativeMethods.HWND_BROADCAST,
+                        NativeMethods.WM_SHOWME,
+                        IntPtr.Zero,
+                        IntPtr.Zero);
+                    Environment.Exit(0);
+                }
+            };
+        }
+
+        private Mutex _mutex;
     }
 }
--- WinPowerHelper.Wpf/MainWindow.xaml.cs
+++ WinPowerHelper.Wpf/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿namespace WinPowerHelper
 {
+    using System;
     using System.Windows;
+    using System.Windows.Interop;
+    using WinPowerHelper.Wpf.Interaction;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -10,6 +13,20 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var source = PresentationSource.FromVisual(this) as HwndSource;
+            source.AddHook(WndProc);
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam, ref bool handled)
+        {
+            if (msg == NativeMethods.WM_SHOWME)
+                Activate();
+            return IntPtr.Zero;
         }
     }
 }
